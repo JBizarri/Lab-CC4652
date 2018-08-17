fe7baeb8b83e300e6d24fdee501244c6218bec6a
Jefferson Bizarri <jefferson.bizarri@gmail.com>
2018-08-17 16:54:33 -0300

    Add files via upload

--- CC4652_Aula01/main.cpp
+++ CC4652_Aula01/main.cpp
@@ -34,27 +34,34 @@ public:
 
     bool remove(int value){
         if (n<=0){
+            cout <<"Erro: Vetor vazio, impossivel remover" << endl;
             return false;
         }
         int i;
-        for(i=0;i<n && v[i]<value;i++);
+        for(i=0;i<n && v[i]!=value;i++);
 
-        for(int j=i;j<n;j++){
-            v[j] = v[j+1];
+        for(int j=i;j<n;j++) {
+            v[j] = v[j + 1];
         }
-        n--;
+        if(i>=n){
+            cout << "Erro: Valor nao encontrado" << endl;
+            return false;
+        }else {
+            cout << "Valor removido: "<<value<<endl;
+            n--;
+        }
         return true;
     }
 
     bool search(int value){
         if (n<=0){
-            cout << "Vetor vazio" << endl;
+            cout << "Erro: Vetor vazio, impossivel encontrar numero" << endl;
             return false;
         }
         int i;
         for(i=0;i<n && v[i]!=value;i++);
         if(i>=n){
-            cout << "Valor nao encontrado" << endl;
+            cout << "Erro: Valor nao encontrado na busca" << endl;
             return false;
         }else {
             cout << "v[" << i <<"] = " << value << endl;
@@ -63,6 +70,10 @@ public:
     }
 
     void print() const{
+        if(n<=0){
+            cout<<"Erro: Vetor vazio, impossivel imprimir"<<endl;
+            return;
+        }
         for(int i=0;i<n;i++){
             cout << v[i] << " ";
         }
@@ -78,6 +89,7 @@ int main() {
     LES v;
     //cout<< v.getN();
     v.search(15);
+    v.remove(15);
     v.insert(1);
     v.insert(10);
     v.insert(-7);
@@ -86,9 +98,16 @@ int main() {
     //cout<< v.getN();
     v.search(1);
     v.remove(1);
+    cout<< v;
+    v.remove(-7);
     //cout<< v.getN();
     cout<< v;
+    v.remove(10);
+    cout<< v;
     v.search(1);
+    v.remove(5);
+    v.search(999);
+    cout<<v;
 
 
     return 0;
